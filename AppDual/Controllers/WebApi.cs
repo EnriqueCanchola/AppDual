abff4bbc43fab96f385a5d0b61dccf91a5e79c08
enrique <canchola@outlook.com>
2019-04-22 17:15:38 -0500

    nada

---
+++ AppDual/Controllers/WebApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AppDual.Controllers
+{
+    public static class WebApi
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+        static WebApi()
+        {
+            WebApiClient.BaseAddress = new Uri("http://wsiep.azurewebsites.net/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplicattion/Json"));
+        }
+    }
+}
