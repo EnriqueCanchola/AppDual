c8dfd6bb406bc77aefb1f5529ee6e6c766062b1a
christian lugo <christian86360@gmail.com>
2019-04-22 17:42:22 -0500

    nuevo login

---
+++ AppDual/Controllers/AlumnoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoDualTese.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Registro_Personal_RH()
+        {
+            return View();
+        }
+
+
+        public IActionResult Publicar()
+        {
+            return View();
+        }
+    }
+}
